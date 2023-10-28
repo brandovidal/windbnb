7c96e3f7e7235e4c22dce31d8d0f727d7d3ff2e2
Brando Vidal <yisusdracon@gmail.com>
2023-10-28 13:14:57 -0500

    ⚒  add style and logo

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -12,7 +12,9 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      { allowConstantExport: true }
     ],
-  },
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn'
+  }
 }
